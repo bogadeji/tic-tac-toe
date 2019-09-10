10a6e4fb1eca45d2996f90ed21977d60126f8f32
igorashs <igorashsirbu@gmail.com>
2019-09-10 11:36:10 +0300

    Add display controller

--- scripts/app.js
+++ scripts/app.js
@@ -64,11 +64,107 @@ const playerFactory = (nickname, choice) => {
     _switchParentWrapper(nickNames, game);
     // TODO
     const gameBoard = ((firstPlayer, secondPlayer) => {
-      function _renderBoard() {}
-
       let board = ['', '', '', '', '', '', '', '', ''];
-      console.log(firstPlayer.getNickname(), secondPlayer.getNickname());
+
+      const firstPlCh = firstPlayer.getChoice();
+      const secondPlCh = secondPlayer.getChoice();
+
+      let currentPlCh = null;
+
+      function _playNextPlayer() {
+        if (currentPlCh == firstPlCh) {
+          currentPlCh = secondPlCh;
+        } else {
+          currentPlCh = firstPlCh;
+        }
+      }
+
+      function getCurrentPlayerCh() {
+        _playNextPlayer();
+        return currentPlCh;
+      }
+
+      function getNameOfPlayer(player) {
+        if (player == 'first') {
+          return firstPlayer.getNickname();
+        }
+        if (player == 'second') {
+          return secondPlayer.getNickname();
+        }
+      }
+
+      function getValueOf(index) {
+        return board[index];
+      }
+      function setValueOf(index) {
+        board[index] = currentPlCh;
+        console.log(board);
+      }
+      return { getValueOf, setValueOf, getCurrentPlayerCh, getNameOfPlayer };
     })(firstPlayer, secondPlayer);
+
+    const displayController = ((gb) => {
+      const board = document.querySelector('.game-board');
+
+      function _currentBoardHandler(e) {
+        if (e.target.classList.contains('content-element')) {
+          _setPlayerChoice(e.target);
+        }
+      }
+
+      function _setPlayerChoice(element) {
+        const span = element.querySelector('span');
+        if (span.textContent == '') {
+          span.textContent = gb.getCurrentPlayerCh();
+          gb.setValueOf(element.dataset.id);
+        }
+      }
+
+      function _renderNickNames() {
+        const fName = document.querySelector(
+          '.player-info.first-pl .nick-name'
+        );
+        const sName = document.querySelector(
+          '.player-info.second-pl .nick-name'
+        );
+        fName.textContent = gb.getNameOfPlayer('first');
+        sName.textContent = gb.getNameOfPlayer('second');
+      }
+
+      function _renderBoard() {
+        board.querySelectorAll('.content-element').forEach((elm, i) => {
+          elm.querySelector('span').textContent = gb.getValueOf(i);
+        });
+      }
+
+      function _setClearGameHandler() {
+        function _clearHandler(e) {
+          board.removeEventListener('click', _currentBoardHandler);
+          e.target.removeEventListener('click', _clearHandler);
+        }
+
+        document.querySelectorAll('.new-game').forEach((newGameBtn) => {
+          newGameBtn.addEventListener('click', _clearHandler);
+        });
+        document.querySelectorAll('.reset-game').forEach((resetGameBtn) => {
+          resetGameBtn.addEventListener('click', _clearHandler);
+        });
+      }
+
+      function _createNewHandler() {
+        _setClearGameHandler();
+        board.addEventListener('click', _currentBoardHandler);
+      }
+
+      function renderNewGame() {
+        _createNewHandler();
+        _renderNickNames();
+        _renderBoard();
+      }
+      return { renderNewGame };
+    })(gameBoard);
+
+    displayController.renderNewGame();
   }
 
   gameMode.querySelector('.pvp-mode').addEventListener('click', () => {
